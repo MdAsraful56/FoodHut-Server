adddb28894490dba08f7ceddc1685e81e5bab03c
MdAsraful56 <work.mdasraful56@gmail.com>
2025-04-28 13:51:52 +0600

    backend jwt is working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -37,8 +38,30 @@ async function run() {
     const reviewCollection = client.db('FoodHutDB').collection('reviews');
     const cartsCollection = client.db('FoodHutDB').collection('carts');
 
+    // jwt related API 
+    app.post('/jwt', async(req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: '1h'
+        });
+        res.send({token});
+    })
 
-
+    const verifyToken = (req, res, next) => {
+        console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+            return res.status(401).send({message: 'forbidden access'});
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(401).send({message: 'forbidden access'});
+            }
+            req.decoded = decoded;
+            next();
+        })
+        // next();
+    }
 
     // users relataed API 
     app.post('/users', async(req, res) => {
@@ -47,9 +70,24 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyToken, async(req, res) => {
+        // console.log(req.headers);
         const result = await userCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/users/admin/:email', verifyToken, async(req, res) => {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+            return res.status(403).send({message: 'unauthorized access'})
+        }
+        const query = { email: email }
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if (user) {
+            admin = user?.role === 'admin';
+        }
+        res.send({admin});
     })
 
     app.delete('/users/:id', async(req, res) => {
